2181c34b3cd17cbf4df58a8b8d48000da8b5ac17
Kris Moore <kris@ixsystems.com>
2018-10-26 14:30:25 -0400

    Initial drop of code, mostly just an internal WS server / client now for testing

---
+++ main.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+// Setup our CLI Flags
+var checkflag bool
+var startflag bool
+var websocketflag bool
+func init() {
+	flag.BoolVar(&checkflag, "check", false, "Check system status")
+	flag.BoolVar(&startflag, "start", false, "Start and upgrade to latest packages")
+	flag.BoolVar(&websocketflag, "websocket", false, "Start websocket server for direct API access and events")
+	flag.Parse()
+}
+
+func startcheck() {
+	data := map[string]string{
+		"method": "check",
+		"action": "testing",
+	}
+	msg, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		log.Fatal("Failed encoding JSON:", err)
+	}
+	fmt.Println("JSON Message: ", string(msg))
+	send_err := c.WriteMessage(websocket.TextMessage, msg)
+	if send_err != nil {
+		log.Fatal("Failed talking to WS backend:", send_err)
+	}
+	time.Sleep(2 * time.Second);
+}
+
+func startws() {
+        log.SetFlags(0)
+        http.HandleFunc("/ws", echo)
+        log.Fatal(http.ListenAndServe(*addr, nil))
+}
+
+var (
+        c   *websocket.Conn
+)
+func connectws() {
+	time.Sleep(2 * time.Second);
+	log.SetFlags(0)
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+
+	u := url.URL{Scheme: "ws", Host: *addr, Path: "/ws"}
+	log.Printf("connecting to %s", u.String())
+
+	err := errors.New("")
+	c, _, err = websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		time.Sleep(200 * time.Millisecond);
+		c, _, err = websocket.DefaultDialer.Dial(u.String(), nil)
+		if err != nil {
+			log.Fatal("dial:", err)
+		}
+	}
+
+	done := make(chan struct{})
+
+	go func() {
+		defer close(done)
+		for {
+			_, message, err := c.ReadMessage()
+			if err != nil {
+				log.Println("read:", err)
+				return
+			}
+			log.Printf("client-recv: %s", message)
+		}
+	}()
+}
+
+// Called when we want to signal that its time to close the WS connection
+func closews() {
+	log.Println("Closing WS connection")
+	defer c.Close()
+
+	// Cleanly close the connection by sending a close message and then
+	// waiting (with timeout) for the server to close the connection.
+	err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+	if err != nil {
+		log.Println("write close:", err)
+		return
+	}
+
+	// This can go away eventually when the echo stops
+	time.Sleep(1 * time.Second);
+}
+
+func main() {
+	if len(os.Args) == 1 {
+		flag.Usage()
+	}
+
+	if ( checkflag ) {
+		go startws()
+		connectws()
+		startcheck()
+		closews()
+		os.Exit(0)
+	}
+	if ( websocketflag ) {
+		startws()
+	}
+}
---
+++ ws.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var (
+	addr    = flag.String("addr", "127.0.0.1:8134", "Websocket service address")
+)
+
+var upgrader = websocket.Upgrader{} // use default options
+
+func echo(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+	defer c.Close()
+	for {
+		mt, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("server-recv: %s", message)
+		err = c.WriteMessage(mt, message)
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+	}
+}
